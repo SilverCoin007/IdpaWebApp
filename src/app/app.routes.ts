e1127415a78262adc0de2d52b8849365b4cec8b2
Silvan Stöckli <silvanstoeckli05@gmail.com>
2024-09-09 12:48:48 +0200

    Finished website

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,6 @@ import { AboutComponent } from './components/about/about.component';
 import { TeamComponent } from './components/team/team.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'team', component: TeamComponent }
